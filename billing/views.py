88d00321aa2293d3fe27b7f4487224c6eae24311
Lalit Joshi <lalit2055joshi@gmail.com>
2023-01-16 15:43:34 +0545

    first commit

--- billing/views.py
+++ billing/views.py
@@ -2,59 +2,69 @@ from django.shortcuts import render,HttpResponseRedirect,redirect
 from .models import *
 from .forms import CustomerRegistration,SubcriptionRegistration
 from email.message import EmailMessage
+from django.contrib.auth.models import Group
 # from BMS import password
 import ssl
 import smtplib
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-@login_required(login_url='signin')
+# @login_required(login_url='signin')
 def add_show(request):   
     cust = Customer.objects.all() 
     return render(request,'billing/addandshow.html',{'cus':cust})
 
 
 def add_customer(request):
-    if request.method=='POST':
-        fm = CustomerRegistration(request.POST)
-        if fm.is_valid():
-            nm = fm.cleaned_data['name']
-            em = fm.cleaned_data['email']
-            stat=fm.cleaned_data['status']
-            phon=fm.cleaned_data['phone']
-            reg = Customer(name=nm,email=em,status=stat,phone=phon)
-
-            reg.save()
+    if request.user.groups.filter(permissions__name='Can add customer'):
+        if request.method=='POST':
             fm = CustomerRegistration(request.POST)
+            if fm.is_valid():
+                nm = fm.cleaned_data['name']
+                em = fm.cleaned_data['email']
+                stat=fm.cleaned_data['status']
+                phon=fm.cleaned_data['phone']
+                reg = Customer(name=nm,email=em,status=stat,phone=phon)
+                reg.save()
+                fm = CustomerRegistration(request.POST)
+        else:
+            fm=CustomerRegistration()   
     else:
-        fm=CustomerRegistration()   
-    # cust = Customer.objects.all() 
+        messages.success(request,"Unauthorized")
+        return redirect('/') 
     return render(request,'billing/createcustomer.html',{'form':fm})
 
 
 def update_data(request,id):
-    pi = Customer.objects.get(pk=id)
-    fm = CustomerRegistration(request.POST,instance=pi)
-    if request.method == 'POST':
-        pi = Customer.objects.get(pk=id)
-        fm = CustomerRegistration(request.POST,instance=pi)
-        fm.save()
-        if fm.is_valid():
-            fm.save()     
+    # import pdb;pdb.set_trace()
+    if request.user.groups.filter(permissions__name='Can change customer'):
+        if request.method == 'POST':
+            pi = Customer.objects.get(pk=id)
+            fm = CustomerRegistration(request.POST,instance=pi)
+            fm.save()
+            if fm.is_valid():
+                fm.save()     
         else:
             pi = Customer.objects.get(pk=id)
-            fm = CustomerRegistration(instance=pi)    
+            fm = CustomerRegistration(instance=pi) 
+    else:
+        messages.success(request,"you are not authorized")
+        return redirect('/')         
     return render(request,"billing/updatecustomer.html", {"form":fm})
 
 
 #THis function will Delete 
 def delete_data(request,id):
-    if request.method == "POST":
-        pi = Customer.objects.get(pk=id)
-        pi.delete()
-        return HttpResponseRedirect('/')
-
+    if request.user.groups.filter(permissions__name='Can change customer'):
+        if request.method == "POST":
+            pi = Customer.objects.get(pk=id)
+            pi.delete()
+            return HttpResponseRedirect('/')
+    else:
+        messages.success(request,"you are not authorized")
+        return redirect('/')
 
 def getactivecustomer(request):
     activecustomer=Customer.objects.filter(status=ACTIVE)
@@ -102,47 +112,54 @@ def sendemail(request):
         smtp.sendmail(email_sender,email_receiver,em.as_string())
     return render(request,'billing/Emailmessage.html')
 
-@login_required(login_url='addshow')
+@login_required(login_url='addandshow')
 def add_show_customer(request):
-    if request.method=='POST':
-        fom = SubcriptionRegistration(request.POST)
-        if fom.is_valid():
-            nm = fom.cleaned_data['customer_name']
-            td=fom.cleaned_data['to_date']
-            amt=fom.cleaned_data['amount']
-            stat=fom.cleaned_data['status']
-            cust=fom.cleaned_data['customer']
-            reg = Subcription(customer_name=nm,to_date=td,amount=amt,status=stat,customer=cust)
-            reg.save()
+    if request.user.groups.filter(permissions__name='Can change subcription'):
+        if request.method=='POST':
             fom = SubcriptionRegistration(request.POST)
+            if fom.is_valid():
+                nm = fom.cleaned_data['customer_name']
+                td=fom.cleaned_data['to_date']
+                amt=fom.cleaned_data['amount']
+                stat=fom.cleaned_data['status']
+                cust=fom.cleaned_data['customer']
+                reg = Subcription(customer_name=nm,to_date=td,amount=amt,status=stat,customer=cust)
+                reg.save()
+                fom = SubcriptionRegistration(request.POST)
+        else:
+            fom=SubcriptionRegistration()   
+        sub = Subcription.objects.all()
     else:
-        fom=SubcriptionRegistration()   
-    sub = Subcription.objects.all() 
+        return redirect('/') 
     return render(request,'billing/addandshowsubscriber.html',{'form':fom,'sub':sub})
 
 
 
 def update_customer_data(request,id):
-    pi = Subcription.objects.get(pk=id)
-    fm = SubcriptionRegistration(request.POST,instance=pi)
-    if request.method == 'POST':
+    if request.user.groups.filter(permissions__name='can change subcription'):
         pi = Subcription.objects.get(pk=id)
         fm = SubcriptionRegistration(request.POST,instance=pi)
-        fm.save()
-        if fm.is_valid():
-            fm.save()     
-        else:
+        if request.method == 'POST':
             pi = Subcription.objects.get(pk=id)
-            fm = SubcriptionRegistration(instance=pi)    
+            fm = SubcriptionRegistration(request.POST,instance=pi)
+            fm.save()
+            if fm.is_valid():
+                fm.save()     
+            else:
+                pi = Subcription.objects.get(pk=id)
+                fm = SubcriptionRegistration(instance=pi) 
+    else:
+        return redirect('/')   
     return render(request,"billing/updatesubscriber.html", {"form":fm})
 
 
 # #THis function will Delete 
 def delete_customer_data(request,id):
-    if request.method == "POST":
-        de = Subcription.objects.get(pk=id)
-        de.delete()
-        return HttpResponseRedirect('addandshowcustomer')
+    if request.user.groups.filter(permissions__name='can delete subcription'):
+        if request.method == "POST":
+            de = Subcription.objects.get(pk=id)
+            de.delete()
+    return redirect('addandshowcustomer')
 
 
 def getpaidsubscriber(request):
@@ -157,9 +174,11 @@ def get_signup(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user=form.save()
+            group = Group.objects.get(name='Customer')
+            user.groups.add(group)
             username = form.cleaned_data.get("username")
-            raw_password = form.cleaned_data.get("password")
+            raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect("/")
@@ -184,4 +203,4 @@ def get_signin(request):
 
 def logoutpage(request):
     logout(request)
-    return redirect("signin")
+    return redirect("addandshow")
